1f56a00b1a7eab7fcb7c4d7d7f7937d6cb29f326
ChWegner <chwegner@pm.me>
2019-02-25 15:49:36 +0100

    first

---
+++ Locker/Db.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SQLite;
+using System.Windows;
+using System.Windows.Documents;
+using static Locker.LockerMain;
+using static Locker.MainPage;
+
+namespace Locker
+{
+    class Db
+    {
+        private SQLiteConnection dbCon;
+
+        public Db()
+        {
+            try
+            {
+                dbCon = new SQLiteConnection("Data Source=locker.db");
+                dbCon.Open();
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.Message);
+            }
+        }
+
+
+        public Boolean VerifyPassword(string username, string password)
+        {
+
+            Boolean verified = false;
+
+            string sql = "SELECT * FROM admin WHERE username = '" + username + "' AND password = '" + password + "'";
+            SQLiteCommand cmd = new SQLiteCommand(sql, dbCon);
+            SQLiteDataReader reader = cmd.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                verified = true;
+            }
+
+
+            return verified;
+
+
+        }
+
+        public ArrayList SearchResults(string chars)
+        {
+
+
+            ArrayList results = new ArrayList();
+            results.Clear();
+            string search = chars + "%";
+
+            string sql = "SELECT t1.website AS website, t2.username AS username, t2.email AS email, t2.change AS change " +
+                         "FROM pages AS t1, logins AS t2 " +
+                         "WHERE t1.ID = t2.websiteId ";
+            if (!chars.Equals(""))
+            {
+                sql = sql + "AND website LIKE '" + search + "'";
+            }
+            SQLiteCommand cmd = new SQLiteCommand(sql, dbCon);
+            SQLiteDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                var result = new WebsiteInfo();
+                result.Website = (string)reader["website"];
+                result.Username = (string)reader["username"];
+                result.Email = (string)reader["email"];
+                result.ChangeDate = (string)reader["change"];
+
+                results.Add(result);
+            }
+
+
+            return results;
+        }
+
+
+
+    }
+}
---
+++ Locker/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+
+
+namespace Locker
+{
+    /// <summary>
+    /// Interaktionslogik für MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        string search;
+        ArrayList list;
+        Db db = new Db();
+
+        public MainPage()
+        {
+            InitializeComponent();
+            start();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            list.Clear();
+            websiteTable.Items.Clear();
+            search = searchText.Text;
+            start(search);
+        }
+
+        public void start(string search = "")
+        {
+            list = db.SearchResults(search);
+
+            foreach (WebsiteInfo item in list)
+            {
+                websiteTable.Items.Add(item);
+            }
+        }
+
+
+        public class WebsiteInfo
+        {
+            public string Website { get; set; }
+            public string Username { get; set; }
+            public string Email { get; set; }
+            public string ChangeDate { get; set; }
+        }
+    }
+}
